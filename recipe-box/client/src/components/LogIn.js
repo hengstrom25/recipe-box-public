d54ebace4e1607d775bfab367ac8a861859ab92d
Heidi <hengstrom25@gmail.com>
2019-07-28 17:18:05 -0400

    create log in component

---
+++ recipe-box/client/src/components/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
+
+const LogIn = () => {
+
+	return (
+		<div>
+			<p className="description_small">Please Log In!</p>
+		</div>
+			)
+}
+
+export default LogIn;
